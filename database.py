70605857b0c3b0dd790684f10aef32d4f983cfdb
shane <shane.a.monaghan@gmail.com>
2023-07-12 20:53:36 -0400

    Initial commit; created database and a view all function

---
+++ database.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+
+def create_database():
+    conn = sqlite3.connect("library.db")
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS books
+                    (
+                    title TEXT NOT NULL,
+                    author TEXT NOT NULL,
+                    genre TEXT NOT NULL,
+                    progress TEXT NOT NULL
+                    )
+                    """)
+    cursor.close()
+    conn.close()
+
+
+def add_book(title, author, genre, progress):
+    conn = sqlite3.connect("library.db")
+    cursor = conn.cursor()
+    insert_tuple = (title, author, genre, progress)
+    cursor.execute("""INSERT INTO books (title, author, genre, progress)
+                   VALUES (?, ?, ?, ?)""", insert_tuple)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def view_all():
+    conn = sqlite3.connect("library.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM books")
+    for row in cursor:
+        print(row)
+    cursor.close()
+    conn.close()
---
+++ main.py
@@ -0,0 +1,5 @@
+from database import *
+
+create_database()
+#add_book('Harry Potter', 'JK Rowling', 'Fantasy', 'TBR')
+view_all()
