a6c09798640d8c1dfd9c0e494b9a5a52724aa8b0
shane <shane.a.monaghan@gmail.com>
2023-07-12 22:03:38 -0400

    Combined functions to get input and add an entry

--- get_input.py
+++ get_input.py
@@ -26,8 +26,9 @@ def get_genre():
     }
     for key in genre_dict.keys():
         print(key + ". " + genre_dict[key])
-    while genre.strip() not in genre_dict.keys():
+    while genre not in genre_dict.keys():
         genre = input("Enter the number of the book's genre: ")
+        genre = genre.strip()
     genre = genre_dict[genre]
     return genre
 
@@ -42,7 +43,22 @@ def get_progress():
     }
     for key in progress_dict.keys():
         print(key + ". " + progress_dict[key])
-    while progress.strip() not in progress_dict.keys():
+    while progress not in progress_dict.keys():
         progress = input("Enter the number of the book's genre: ")
+        progress = progress.strip()
     progress = progress_dict[progress]
     return progress
+
+
+def get_continue():
+    to_continue = ""
+    print("1.", "Continue")
+    print("2.", "Return to menu")
+    while to_continue != "1" and to_continue != "2":
+        to_continue = input("Enter the number of what you would like to do: ")
+        to_continue = to_continue.strip()
+    if to_continue == "1":
+        return True
+    return False
+
+
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from database import *
 from get_input import *
 
-
 def get_entry_input():
+    print("\n"*100)
     title = get_title()
     author = get_author()
+    print("Genres")
     genre = get_genre()
+    print("Book Status")
     progress = get_progress()
+    print("")
     add_entry(title, author, genre, progress)
+    print("Would you like to continue adding books?")
+    if get_continue():
+        get_entry_input()
+
+
+get_entry_input()
+view_all()
