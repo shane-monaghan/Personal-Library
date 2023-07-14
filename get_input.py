d7a26a33e38b614225a40d50c7cdb1035d931233
shane <shane.a.monaghan@gmail.com>
2023-07-13 22:37:07 -0400

    Added main menu and search

--- get_input.py
+++ get_input.py
@@ -1,6 +1,7 @@
 from os import system, name
 
 
+# For adding a book entry
 def get_title():
     title = ""
     while title.strip() == "":
@@ -47,12 +48,13 @@ def get_progress():
     for key in progress_dict.keys():
         print(key + ". " + progress_dict[key])
     while progress not in progress_dict.keys():
-        progress = input("Enter the number of the book's current state: ")
+        progress = input("Enter the number of the book's current status: ")
         progress = progress.strip()
     progress = progress_dict[progress]
     return progress
 
 
+# Continue option
 def get_continue():
     to_continue = ""
     print("1.", "Continue")
@@ -65,19 +67,46 @@ def get_continue():
     return False
 
 
+# For the main menu
 def get_menu_choice():
     choice = ""
-    choices = ["1", "2", "3", "4"]
+    choices = ["1", "2", "3", "4", "5"]
     print("1.", "Add a Book")
     print("2.", "Search for a Book")
     print("3.", "Update a Book")
     print("4.", "Delete a Book")
+    print("5.", "Quit")
     while choice not in choices:
         choice = input("Enter the number of what you would like to do: ")
         choice = choice.strip()
     return choice
 
 
+# For searching for books
+def get_category():
+    category = ""
+    category_dict = {"1":"title",
+                     "2":"author",
+                     "3":"genre",
+                     "4":"progress"
+                     }
+    print("1.", "Title")
+    print("2.", "Author")
+    print("3.", "Genre")
+    print("4.", "Book Status")
+    while category not in category_dict.keys():
+        category = input("Enter the number of what you'd like to search by: ")
+        category = category.strip()
+    return category_dict[category]
+
+
+def get_search():
+    search = ""
+    search = input("Enter your search, enter nothing to view all: ")
+    return search
+
+
+# Clears the screen in between actions
 def clear():
     if name == 'nt':
         _ = system('cls')
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from get_input import *
 
 def main_menu():
     clear()
-
+    choice = get_menu_choice()
+    if choice == "1":
+        get_entry_input()
+    elif choice == "2":
+        search_book()
 
 def get_entry_input():
     clear()
@@ -19,7 +23,27 @@ def get_entry_input():
     print("Would you like to continue adding books?")
     if get_continue():
         get_entry_input()
+    else:
+        main_menu()
 
 
-get_entry_input()
+def search_book():
+    clear()
+    print("Attributes")
+    category = get_category()
+    print("Search")
+    search = get_search()
+    clear()
+    if search == "":
+        view_all()
+    else:
+        view_by(category, search)
+    print("")
+    if get_continue():
+        search_book()
+    else:
+        main_menu()
+
+
+main_menu()
 view_all()
